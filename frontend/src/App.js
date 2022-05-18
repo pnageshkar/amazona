81abd27dbc3eab9f7ead915c3d171dea7abe4eec
Prasad Nageshkar <prasad.nageshkar@gmail.com>
2022-05-18 13:09:22 +0530

    Stage 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,38 @@
+//import data from "./data";
+import {BrowserRouter,Routes, Route, Link} from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from  'react-router-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <a href ="/">amazona</a>
-      </header>
-      <main>
-        list products
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className='d-flex flex-column site-container'>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>amazona</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
+        
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
